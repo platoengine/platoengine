a7e7d317e75a6f2782ee9fe34309eb58e89cae60
jrobbin <jrobbin@sandia.gov>
2019-08-22 11:42:41 -0600

    Added gradX test for Stabilized Mechanics

---
+++ examples/ComputeGradientX_StabMech/computeGradientX.py
@@ -0,0 +1,36 @@
+import exodus
+import Plato
+
+# boilerplate that dynamically loads the mpi library required by Plato.Analyze
+import ctypes
+ctypes.CDLL("libmpi.so",mode=ctypes.RTLD_GLOBAL)
+
+# create global Analyze instance
+analyze = Plato.Analyze("InternalEnergyGradX_input.xml", "InternalEnergyGradX_appfile.xml", "3D bolted bracket")
+analyze.initialize();
+
+outMesh = exodus.ExodusDB()
+outMesh.read("bolted_bracket.exo");
+numNodes = outMesh.numNodes;
+
+# compute gradientX
+vals = [1.0 for i in range(numNodes)];
+analyze.importData("Topology", "SCALAR_FIELD", vals)
+analyze.compute("Compute ObjectiveX")
+solx  = analyze.exportData("Solution X", "SCALAR_FIELD")
+soly  = analyze.exportData("Solution Y", "SCALAR_FIELD")
+solz  = analyze.exportData("Solution Z", "SCALAR_FIELD")
+adjx  = analyze.exportData("Adjoint X", "SCALAR_FIELD")
+adjy  = analyze.exportData("Adjoint Y", "SCALAR_FIELD")
+adjz  = analyze.exportData("Adjoint Z", "SCALAR_FIELD")
+gradx = analyze.exportData("Objective GradientX X", "SCALAR_FIELD")
+grady = analyze.exportData("Objective GradientX Y", "SCALAR_FIELD")
+gradz = analyze.exportData("Objective GradientX Z", "SCALAR_FIELD")
+
+# open exodus file for output and configure
+numTimes = 1
+outMesh.nodeVarNames = ["solution_x", "solution_y", "solution_z", "adjoint_x", "adjoint_y", "adjoint_z", "gradientx_x", "gradientx_y", "gradientx_z"]
+outMesh.numNodeVars = len(outMesh.nodeVarNames)
+outMesh.nodeVars = [[solx,soly,solz,adjx,adjy,adjz,gradx,grady,gradz]]
+outMesh.varTimes = [1.0]
+outMesh.write("InternalEnergyGradX.exo")
