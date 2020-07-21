a50da7118ed3f1962ec7b67b1273b8a0b0d0bd7b
Joshua Robbins <jrobbin@sandia.gov>
2020-07-21 14:53:28 -0600

    fixed parabolic

--- examples/Analyze_Mitchell2D_MLS/opt.py
+++ examples/Analyze_Mitchell2D_MLS/opt.py
@@ -9,7 +9,7 @@ from numpy import *
 ctypes.CDLL("libmpi.so",mode=ctypes.RTLD_GLOBAL)
 
 # create global Analyze instance
-appFileName = "alexaApp.xml"
+appFileName = "analyzeApp.xml"
 defaultInputFile = "mitchell_tri.xml"
 analyze = PlatoPython.Analyze(defaultInputFile, appFileName, "2D mitchell")
 analyze.initialize();
